e5269f80d279ff90546ebbe406bcec3eaab9106b
z18080121557 <51199824+z18080121557@users.noreply.github.com>
2026-04-08 18:00:07 +0800

    Delete jfxt directory

--- jfxt/src/main/webapp/static/js/wbCommon.js
+++
@@ -1,8 +0,0 @@
-
- function col() {
-	 document.getElementById('imgContent').style.display='none';
- } 
- function showImg(w) {
-	 document.getElementById('imgCon').src = w;
-	 document.getElementById('imgContent').style.display='block';
- } 
